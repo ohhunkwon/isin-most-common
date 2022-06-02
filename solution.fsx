6d60cb63591bc4df00877cf28b627ec883284139
PY Rivaille <py.rivaille@gmail.com>
2022-06-02 20:20:20 +0100

    Update solution.fsx

--- solution.fsx
+++ solution.fsx
@@ -1,20 +1,12 @@
 #r "nuget: FSharp.Data"
 
 open FSharp.Data
-open System.Collections.Generic
 
 let file = "/BV_TWEM_20220531_1514.csv"
 
-let csv = CsvFile.Load(__SOURCE_DIRECTORY__ + file, skipRows=6, ignoreErrors=true).Cache()
+let csv = CsvFile.Load(__SOURCE_DIRECTORY__ + file, skipRows=6, ignoreErrors=true)
 
-let dict = new Dictionary<string, int>()
-
-for row in csv.Rows do
-    // printfn "ISIN: (%s)"
-    //     (row.GetColumn "ISIN")
-    let isin = row.GetColumn "ISIN"
-    if not (dict.ContainsKey(isin)) then dict.Add(isin, 1) else dict.[isin] <- dict.[isin] + 1
-    
-dict
-|> Seq.sortBy (fun (KeyValue(k,v)) -> v)
-|> Seq.iter (fun (KeyValue(k,v)) -> printfn "%s: %d" k v)
+csv.Rows
+|> Seq.countBy (fun csvRow -> csvRow.GetColumn "ISIN")
+|> Seq.sortBy snd
+|> Seq.iter (fun (k, v) -> printfn "%s: %d" k v)
