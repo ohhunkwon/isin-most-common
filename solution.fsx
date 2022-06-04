c4a632a78bdbdf1768eaef2715c07ebae47aaa81
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-06-04 10:36:42 +0100

    fix: Seq.Fold implementation

--- solution.fsx
+++ solution.fsx
@@ -6,7 +6,20 @@ let file = "/BV_TWEM_20220531_1514.csv"
 
 let csv = CsvFile.Load(__SOURCE_DIRECTORY__ + file, skipRows=6, ignoreErrors=true)
 
-csv.Rows
-|> Seq.countBy (fun csvRow -> csvRow.GetColumn "ISIN")
-|> Seq.sortBy snd
-|> Seq.iter (fun (k, v) -> printfn "%s: %d" k v)
+// csv.Rows
+// |> Seq.countBy (fun csvRow -> csvRow.GetColumn "ISIN")
+// |> Seq.sortBy snd
+// |> Seq.iter (fun (k, v) -> printfn "%s: %d" k v)
+let csvIsin = Seq.map (fun (csvRow : CsvRow) -> csvRow.GetColumn "ISIN") csv.Rows
+
+let csvRowArr = Seq.toArray csvIsin
+
+let addElement (acc:Map<string,int>) element = 
+    if acc.ContainsKey element then acc.Add(element, acc.[element] + 1) 
+    else acc.Add(element, 1)
+
+(Map.empty, csvRowArr)
+||> Seq.fold (addElement)
+|> Seq.sortBy (fun (KeyValue(k,v)) -> v)
+|> Seq.iter (fun (KeyValue(k,v)) -> printfn "%s: %d" k v) 
+
